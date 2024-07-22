02416ad734c5e0e9bb917dbc36d2f48a0328df1a
Meikostocs <cirillocristianpio02@gmail.com>
2024-07-22 10:38:02 +0200

    Sync constraint Added all GPIO constraint

--- mmr-kria-hardware-design.gen/sources_1/bd/pl_design/hdl/pl_design_wrapper.v
+++ mmr-kria-hardware-design.gen/sources_1/bd/pl_design/hdl/pl_design_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2023.2 (lin64) Build 4029153 Fri Oct 13 20:13:54 MDT 2023
-//Date        : Mon Jul 22 01:27:45 2024
+//Date        : Mon Jul 22 10:37:11 2024
 //Host        : cristian-ubuntu running 64-bit Ubuntu 22.04.4 LTS
 //Command     : generate_target pl_design_wrapper.bd
 //Design      : pl_design_wrapper
@@ -38,7 +38,7 @@ module pl_design_wrapper
   output UART_0_0_txd;
   output [0:0]fan;
   inout [1:0]led_uf_tri_io;
-  inout [0:0]rpi_tri_io;
+  inout [6:0]rpi_tri_io;
 
   wire CAN_0_0_rx;
   wire CAN_0_0_tx;
@@ -73,9 +73,33 @@ module pl_design_wrapper
   wire [0:0]led_uf_tri_t_0;
   wire [1:1]led_uf_tri_t_1;
   wire [0:0]rpi_tri_i_0;
+  wire [1:1]rpi_tri_i_1;
+  wire [2:2]rpi_tri_i_2;
+  wire [3:3]rpi_tri_i_3;
+  wire [4:4]rpi_tri_i_4;
+  wire [5:5]rpi_tri_i_5;
+  wire [6:6]rpi_tri_i_6;
   wire [0:0]rpi_tri_io_0;
+  wire [1:1]rpi_tri_io_1;
+  wire [2:2]rpi_tri_io_2;
+  wire [3:3]rpi_tri_io_3;
+  wire [4:4]rpi_tri_io_4;
+  wire [5:5]rpi_tri_io_5;
+  wire [6:6]rpi_tri_io_6;
   wire [0:0]rpi_tri_o_0;
+  wire [1:1]rpi_tri_o_1;
+  wire [2:2]rpi_tri_o_2;
+  wire [3:3]rpi_tri_o_3;
+  wire [4:4]rpi_tri_o_4;
+  wire [5:5]rpi_tri_o_5;
+  wire [6:6]rpi_tri_o_6;
   wire [0:0]rpi_tri_t_0;
+  wire [1:1]rpi_tri_t_1;
+  wire [2:2]rpi_tri_t_2;
+  wire [3:3]rpi_tri_t_3;
+  wire [4:4]rpi_tri_t_4;
+  wire [5:5]rpi_tri_t_5;
+  wire [6:6]rpi_tri_t_6;
 
   IOBUF SPI_0_0_io0_iobuf
        (.I(SPI_0_0_io0_o),
@@ -131,12 +155,42 @@ module pl_design_wrapper
         .led_uf_tri_i({led_uf_tri_i_1,led_uf_tri_i_0}),
         .led_uf_tri_o({led_uf_tri_o_1,led_uf_tri_o_0}),
         .led_uf_tri_t({led_uf_tri_t_1,led_uf_tri_t_0}),
-        .rpi_tri_i(rpi_tri_i_0),
-        .rpi_tri_o(rpi_tri_o_0),
-        .rpi_tri_t(rpi_tri_t_0));
+        .rpi_tri_i({rpi_tri_i_6,rpi_tri_i_5,rpi_tri_i_4,rpi_tri_i_3,rpi_tri_i_2,rpi_tri_i_1,rpi_tri_i_0}),
+        .rpi_tri_o({rpi_tri_o_6,rpi_tri_o_5,rpi_tri_o_4,rpi_tri_o_3,rpi_tri_o_2,rpi_tri_o_1,rpi_tri_o_0}),
+        .rpi_tri_t({rpi_tri_t_6,rpi_tri_t_5,rpi_tri_t_4,rpi_tri_t_3,rpi_tri_t_2,rpi_tri_t_1,rpi_tri_t_0}));
   IOBUF rpi_tri_iobuf_0
        (.I(rpi_tri_o_0),
         .IO(rpi_tri_io[0]),
         .O(rpi_tri_i_0),
         .T(rpi_tri_t_0));
+  IOBUF rpi_tri_iobuf_1
+       (.I(rpi_tri_o_1),
+        .IO(rpi_tri_io[1]),
+        .O(rpi_tri_i_1),
+        .T(rpi_tri_t_1));
+  IOBUF rpi_tri_iobuf_2
+       (.I(rpi_tri_o_2),
+        .IO(rpi_tri_io[2]),
+        .O(rpi_tri_i_2),
+        .T(rpi_tri_t_2));
+  IOBUF rpi_tri_iobuf_3
+       (.I(rpi_tri_o_3),
+        .IO(rpi_tri_io[3]),
+        .O(rpi_tri_i_3),
+        .T(rpi_tri_t_3));
+  IOBUF rpi_tri_iobuf_4
+       (.I(rpi_tri_o_4),
+        .IO(rpi_tri_io[4]),
+        .O(rpi_tri_i_4),
+        .T(rpi_tri_t_4));
+  IOBUF rpi_tri_iobuf_5
+       (.I(rpi_tri_o_5),
+        .IO(rpi_tri_io[5]),
+        .O(rpi_tri_i_5),
+        .T(rpi_tri_t_5));
+  IOBUF rpi_tri_iobuf_6
+       (.I(rpi_tri_o_6),
+        .IO(rpi_tri_io[6]),
+        .O(rpi_tri_i_6),
+        .T(rpi_tri_t_6));
 endmodule
--- mmr-kria-hardware-design.srcs/constrs_1/new/gpio.xdc
+++ mmr-kria-hardware-design.srcs/constrs_1/new/gpio.xdc
@@ -1,7 +1,22 @@
+# RPI constraints
+
+# LEDS
 set_property IOSTANDARD LVCMOS18 [get_ports {led_uf_tri_io}]
-set_property PACKAGE_PIN F8 [get_ports {led_uf_tri_io[0]}]
-set_property PACKAGE_PIN E8 [get_ports {led_uf_tri_io[1]}]
+
+set_property PACKAGE_PIN Y12 [get_ports {led_uf_tri_io[0]}]  # ASSI-B
+set_property PACKAGE_PIN AH14 [get_ports {led_uf_tri_io[1]}] # ASSI-Y
 
 
+# GPIO
 set_property IOSTANDARD LVCMOS33 [get_ports {rpi_tri_io}]
-set_property PACKAGE_PIN AA12 [get_ports {rpi_tri_io[0]}]
+
+set_property PACKAGE_PIN AA12 [get_ports {rpi_tri_io[0]}] # ASMS
+set_property PACKAGE_PIN Y9 [get_ports {rpi_tri_io[1]}]   # SDC-SENS
+set_property PACKAGE_PIN AA8 [get_ports {rpi_tri_io[2]}]  # SDC-CTRL
+set_property PACKAGE_PIN AB10 [get_ports {rpi_tri_io[3]}] # EBS 2
+set_property PACKAGE_PIN AG13 [get_ports {rpi_tri_io[4]}] # EBS-1
+set_property PACKAGE_PIN AB9 [get_ports {rpi_tri_io[5]}]  # WATCHDOG
+set_property PACKAGE_PIN AB14 [get_ports {rpi_tri_io[6]}] # BUZZER
+
+
+
